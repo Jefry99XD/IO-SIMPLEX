df6f4206dbe4e3b142a9adab18f95da06a949897
Jefry99XD <jefrycc99@gmail.com>
2024-03-17 19:15:59 -0600

    esqueleto de la entrada

--- frontend/simplex/src/app/app.component.ts
+++ frontend/simplex/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Simplex con jefry y ariana xd';
+  title = 'Simplex con jefry y ariana';
 }
--- frontend/simplex/src/app/entrada/entrada.component.ts
+++ frontend/simplex/src/app/entrada/entrada.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-entrada',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,
+    FormsModule ],
   templateUrl: './entrada.component.html',
   styleUrl: './entrada.component.css'
 })
 export class EntradaComponent {
+  variables = 0
+  restricciones = 0
+  objetivo = ''
 
 }
